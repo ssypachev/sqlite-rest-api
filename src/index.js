1299cb4cc6591d054dd07de1b6a1397c7255d428
Stepan Sypachev <ssypachev@dynamixsoftware.com>
2021-01-31 02:53:08 +0500

    add dependencies and start server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!');
+const path = require('path');
+//Позднее эта глобальная переменная поможет в работе. Она определяет корень
+//проекта
+global.baseDir = path.resolve(__dirname);
+
+const express = require('express'),
+	  app     = express(); //это экземпляр сервера
+
+require('express-async-errors');
+
+(async () => {
+	const PORT = 3233;
+	
+	await app.listen(PORT);
+	console.log(`Server listening to ${PORT}`);
+})();
+
